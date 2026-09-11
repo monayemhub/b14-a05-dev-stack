053d6b2ad3b65820de49124ac2309d410d18e497
Monayem Kabir Khan <monayemhub@gmail.com>
2026-09-11 20:57:16 +0600

    - Add font - Add Navbar - Add brand theme gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar.tsx";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/brand_theme.ts
@@ -0,0 +1,4 @@
+const brandTheme: string =
+  "bg-linear-to-r from-orange-500 via-pink-500 to-violet-500";
+
+export default brandTheme;
